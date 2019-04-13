ad60201d85f70ab6e2b3fc05cc8ab5cd3b98416e
kelsa-pi <sasakelecevic@gmail.com>
2019-04-13 15:03:15 +0200

    fix re import

--- pyzoPyUNOWorkspace/utils.py
+++ pyzoPyUNOWorkspace/utils.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # PyUNO Workspace helper module
-
+from re import findall
+from os.path import join
 
 def splitName(name):
     """ splitName(name)
     Split an object name in parts, taking dots and indexing into account.
     """
-    name = name.replace('[', '.[')
-    parts = name.split('.')
+    name = name.replace("[", ".[")
+    parts = name.split(".")
     return [p for p in parts if p]
 
 
@@ -17,18 +18,18 @@ def splitNameCleaner(name):
     Object name with extra dots eg. ctx.getByName("/singletons/com.sun.star.beans.theIntrospection"),
     enumerated objects eg. list(document.Text)
     """
-    name = name.replace('[', '.[')
-    parts = name.split('.')
+    name = name.replace("[", ".[")
+    parts = name.split(".")
 
     # Fix extra dots
     if '"' in parts[-1]:
-        extra_dots = re.findall(r'"(.*?)"', name)
+        extra_dots = findall(r'"(.*?)"', name)
         if extra_dots:
             for part in extra_dots:
-                new = part.replace('.', '_')
+                new = part.replace(".", "_")
                 new_name = name.replace(part, new)
 
-            new_parts = new_name.split('.')
+            new_parts = new_name.split(".")
             np = [p for p in new_parts if p]
 
     else:
@@ -36,8 +37,8 @@ def splitNameCleaner(name):
         np = [p for p in parts if p]
 
     # Fix list
-    if np[0].startswith('list(') and np[-1].endswith(')'):
-        np[0] = np[0].replace('list(', '')
+    if np[0].startswith("list(") and np[-1].endswith(")"):
+        np[0] = np[0].replace("list(", "")
         np[-1] = np[-1][:-1]
         np.append(np[-1])
 
@@ -49,13 +50,5 @@ def joinName(parts):
     Join the parts of an object name, taking dots and indexing into account.
     """
 
-    name = '.'.join(parts)
-    return name.replace('.[', '[')
-
-    
-
-
-
-
-    
-
+    name = ".".join(parts)
+    return name.replace(".[", "[")
